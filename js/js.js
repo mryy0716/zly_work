770f5bc933bda9357c4980c2b1b3593ff3f00942
mryy0716 <2582819758@qq.com>
2017-09-12 10:22:05 +0800

    work

--- js/js.js
+++ js/js.js
@@ -1,32 +1,32 @@
 var vm=new Vue({
 	el:"#shenDengxi",
 	data:{
-		flag:{
+		pd:{
 			ok:"errorLabel",
 			lar:false,
 			name:"",
 			nameCon:""
 		},
-		flag1:{
+		pd1:{
 			ok:"errorLabel",
 			lar:false,
 			name:"",
 			nameCon:""
 		},
-		flag2:{
+		pd2:{
 			ok:"errorLabel",
 			lar:false,
 			name:"",
 			nameCon:"",
 			xian:true
 		},
-		flag3:{
+		pd3:{
 			ok:"errorLabel",
 			lar:false,
 			name:"",
 			nameCon:""
 		},
-		flag4:{
+		pd4:{
 			ok:"errorLabel",
 			lar:false,
 			name:"",
@@ -44,42 +44,42 @@ var vm=new Vue({
 		inp(start){
 				switch(true){
 					case start.name.length==0:
-						this.fn("用户名不能为空","errorLabel",true,this.flag);
+						this.fn("用户名不能为空","errorLabel",true,this.pd);
 						break;
 					case start.name.length<6||start.name.length>16:
-						this.fn("用户名要6-16位","errorLabel",true,this.flag);
+						this.fn("用户名要6-16位","errorLabel",true,this.pd);
 						break;
 					case !/^(\w|-|[\u4e00-\u9fa5])*$/.test(start.name):
-						this.fn("请使用汉字、字母、数字组合","errorLabel",true,this.flag);
+						this.fn("请使用汉字、字母、数字组合","errorLabel",true,this.pd);
 						break;
 					default:
-						this.fn("ok","validLabel",true,this.flag);
+						this.fn("ok","validLabel",true,this.pd);
 				}
 		},
 		inp1(start){
 			switch(true){
 					case start.name.length==0:
-						this.fn("手机号不能为空","errorLabel",true,this.flag1);
+						this.fn("手机号不能为空","errorLabel",true,this.pd1);
 						break;
 					case !/^1[34578]\d{9}$/.test(start.name):
-						this.fn("手机号格式错误！","errorLabel",true,this.flag1);
+						this.fn("手机号格式错误！","errorLabel",true,this.pd1);
 						break;
 					default:
-						this.fn("ok","validLabel",true,this.flag1);
+						this.fn("ok","validLabel",true,this.pd1);
 				}
 		},
 		inp2(start) {
 			switch (true) {
 				case start.name.length == 0:
-					this.fn( "验证码不能为空","errorLabel",true,this.flag2);
+					this.fn( "验证码不能为空","errorLabel",true,this.pd2);
 					break;
 				case this.yzbtn:
 					break;
 				case start.name.toString() != this.sj:
-					this.fn( "验证码错误","errorLabel",true,this.flag2);
+					this.fn( "验证码错误","errorLabel",true,this.pd2);
 					break;
 				default:
-					this.fn("ok","validLabel",true,this.flag2);
+					this.fn("ok","validLabel",true,this.pd2);
 			}
 		},
 		yz() {
@@ -87,37 +87,37 @@ var vm=new Vue({
 			this.sj = Math.random().toString().substr(2, 4);
 		},
 		xianshi(){
-			this.flag2.lar=true;
-			this.flag2.xian=false;
+			this.pd2.lar=true;
+			this.pd2.xian=false;
 			this.yz();
 		},
 		inp3(start) {
 			switch (true) {
 				case start.name.length == 0:
-					this.fn("密码不能为空","errorLabel",true,this.flag3);
+					this.fn("密码不能为空","errorLabel",true,this.pd3);
 					break;
 				case !/^[0-9a-z]{6,20}$/.test(start.name):
-					this.fn("密码只能是6-20位的数字或字母","errorLabel",true,this.flag3);
+					this.fn("密码只能是6-20位的数字或字母","errorLabel",true,this.pd3);
 					break;
 				default:
-					this.fn("ok","validLabel",true,this.flag3);
+					this.fn("ok","validLabel",true,this.pd3);
 			}
 		},
 		inp4(start) {
 			switch (true) {
 				case start.name.length == 0:
-					this.fn("确认密码不能为空","errorLabel",true,this.flag4);
+					this.fn("确认密码不能为空","errorLabel",true,this.pd4);
 					break;
-				case this.flag3.name != start.name:
-					this.fn("两次密码不一样！","errorLabel",true,this.flag4);
+				case this.pd3.name != start.name:
+					this.fn("密码错误！","errorLabel",true,this.pd4);
 					break;
 				default:
-					this.fn("ok","validLabel",true,this.flag4);
+					this.fn("ok","validLabel",true,this.pd4);
 			}
 		},
 		btn(){
-			if(this.flag1.ok&&this.flag.ok&&this.flag2.ok&&this.flag3.ok&&this.flag4.ok=="validLabel"){
-				console.log(this.flag1.name,this.flag.name,this.flag2.name,this.flag3.name,this.flag4.name)
+			if(this.pd1.ok&&this.pd.ok&&this.pd2.ok&&this.pd3.ok&&this.pd4.ok=="validLabel"){
+				console.log(this.pd1.name,this.pd.name,this.pd2.name,this.pd3.name,this.pd4.name)
 			}
 		}
 	}
